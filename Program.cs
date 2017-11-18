51feb4b25cadb944177a6fbd793ae15785573dad
Tudor Turcu <tudor.turcu@gmail.com>
2017-11-18 18:36:32 +0200

    Initial .NET Core project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rubikcubesolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from Rubik's cube solver!");
+        }
+    }
+}
