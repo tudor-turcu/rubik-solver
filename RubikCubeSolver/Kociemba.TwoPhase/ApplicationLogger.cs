892dcddcf6e7d993ba17df46b9938bcf19538569
Tudor Turcu <tudor.turcu@gmail.com>
2017-11-26 20:07:29 +0200

    Optimized loading time - cache move tables - added missing files

---
+++ RubikCubeSolver/Kociemba.TwoPhase/ApplicationLogger.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace RubikCubeSolver.Kociemba.TwoPhase
+{
+    public static class ApplicationLogging
+    {
+        public static ILoggerFactory LoggerFactory { get; } = new LoggerFactory().AddConsole(LogLevel.Debug).AddDebug(LogLevel.Debug);
+
+        public static ILogger CreateLogger<T>() =>
+            LoggerFactory.CreateLogger<T>();
+    }
+}
